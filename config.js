c3ed851039d757527dd61f7f0fddab7ad6d61537
Justin Li <justin031607@gmail.com>
2025-10-27 21:32:19 -0500

    feat: update Supabase URL to use custom domain
    - Update config.js to use auth.quadhls.com instead of default Supabase URL
    - Prepare for custom domain authentication flow

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Supabase configuration for outline population
 
 export const config = {
-  supabaseUrl: 'https://ujsnnvdbujguiejhxuds.supabase.co',
+  supabaseUrl: 'https://auth.quadhls.com',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqc25udmRidWpndWllamh4dWRzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NzkwNjMyOSwiZXhwIjoyMDczNDgyMzI5fQ.A8HKj1JrItYPxOiufvtdqr1U_MATQF1clYZGvF63oKY',
   bucketName: 'Outlines',
   bucketFolder: 'out'

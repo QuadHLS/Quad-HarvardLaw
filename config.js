b236f98ee588104f2955ad3ae5fa7957e6cba3e9
Justin Li <justin031607@gmail.com>
2025-10-30 17:49:49 -0500

    Security: remove exposed service role key and add to gitignore

--- config.js
+++
@@ -1,8 +0,0 @@
-// Supabase configuration for outline population
-
-export const config = {
-  supabaseUrl: 'https://auth.quadhls.com',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqc25udmRidWpndWllamh4dWRzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NzkwNjMyOSwiZXhwIjoyMDczNDgyMzI5fQ.A8HKj1JrItYPxOiufvtdqr1U_MATQF1clYZGvF63oKY',
-  bucketName: 'Outlines',
-  bucketFolder: 'out'
-}
